48c80b78cd10880e673baf35380b986a1650bc80
nerd <why@boquaninc.com>
2022-11-24 18:10:51 +0800

    modify

--- setup_docker.sh
+++ setup_docker.sh
@@ -4,8 +4,12 @@ rm -rf /work/bin-satoshicard/satoshicard/circuits
 rm -rf /work/bin-satoshicard/satoshicard/contract
 rm -rf /work/bin-satoshicard/satoshicard/desc
 rm -rf /work/bin-satoshicard/satoshicard/satoshicard
+rm -rf /work/bin-satoshicard/satoshicard/1.sh
+rm -rf /work/bin-satoshicard/satoshicard/2.sh
 
 cp -r circuits /work/bin-satoshicard/satoshicard/circuits
 cp -r contract /work/bin-satoshicard/satoshicard/contract
 cp -r desc /work/bin-satoshicard/satoshicard/desc
 cp satoshicard /work/bin-satoshicard/satoshicard/satoshicard
+cp 1.sh /work/bin-satoshicard/satoshicard/1.sh
+cp 2.sh /work/bin-satoshicard/satoshicard/2.sh
